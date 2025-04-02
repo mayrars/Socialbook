ac029578ed7266e688546be79bd9389afe2549e0
mayrars <mayra203@gmail.com>
2025-04-02 07:10:37 -0600

    adding singup

--- core/models.py
+++ core/models.py
@@ -9,4 +9,7 @@ class Profile(models.Model):
     id_user = models.IntegerField()
     bio= models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.webp')
-    location = models.CharField(max_length=100,blank=True)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
