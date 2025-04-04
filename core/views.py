5f090bd552663963091f02e9a337e8699940b7a1
mayrars <mayra203@gmail.com>
2025-04-04 05:51:17 -0600

    created migration and adding messages

--- core/views.py
+++ core/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
@@ -18,6 +18,19 @@ def signup(request):
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email already taken')
+                return redirect('signup')            
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+
+                return redirect('signup')
         else:
             messages.info(request, 'Password do not match')
             return redirect('signup')
