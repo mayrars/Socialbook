0eabe19cc4981fb0a9ea5312a2dd5cd5f26db8f4
mayrars <mayra203@gmail.com>
2025-04-09 07:07:18 -0600

    adding settings and view for settings

--- core/views.py
+++ core/views.py
@@ -2,11 +2,42 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method =='POST':
+        if request.FILE.get('imagte')==None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        if request.FILES.get('image')!=None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save() 
+
+        return redirect('settings')
+
+    return render(request, 'setting.html',{ 'user_profile': user_profile })
 
 def signup(request):
     if request.method == 'POST':
@@ -25,6 +56,9 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
@@ -52,6 +86,7 @@ def signin(request):
     else:
         return render(request, 'signin.html')
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
